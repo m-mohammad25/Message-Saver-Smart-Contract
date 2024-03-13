e9d1303e4491cfc06c4d0d2fdfd2a5001ae6ef3b
Mohammad Mohammad <m.mohammad25@outlook.com>
2024-03-13 16:38:13 +0200

    after deploying the contract and fixing some issues

---
+++ contracts/MessageSaver.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+import "hardhat/console.sol";
+
+contract MessageSaver {
+    string private message;
+
+    constructor(string memory _msg) {
+        console.log("Deploying a Greeter with greeting:", _msg);
+        message = _msg;
+    }
+
+    function readMsg() public view returns (string memory) {
+        return message;
+    }
+
+    function saveMsg(string memory _msg) public {
+        console.log("Changing greeting from '%s' to '%s'", message, _msg);
+        message = _msg;
+    }
+
+
+}
---
+++ scripts/deploy.ts
@@ -0,0 +1,19 @@
+// import { ethers } from "@nomiclabs/hardhat-ethers";
+import { ethers } from "hardhat";
+
+async function main() {
+  const messageSaver = await ethers.deployContract("MessageSaver", [
+    "Greetings!",
+  ]);
+
+  await messageSaver.waitForDeployment();
+
+  console.log("MessageSaver deployed to:", messageSaver.target);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
